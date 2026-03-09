61b814f05e8848427c91dd3d1746ab3469f6c5a4
geniuxy <2238384910@qq.com>
2026-03-09 18:06:57 +0800

    11 Add the Speed Attribute to Drive Ground Locomotion

---
+++ Source/Crunch/Private/Animations/CAnimInstance.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animations/CAnimInstance.h"
+
+#include "GameFramework/Character.h"
+
+void UCAnimInstance::NativeInitializeAnimation()
+{
+	OwnerPlayerCharacter = Cast<ACharacter>(TryGetPawnOwner());
+	if (OwnerPlayerCharacter)
+	{
+		OwnerMovementComp = OwnerPlayerCharacter->GetCharacterMovement();
+	}
+}
+
+void UCAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	if (OwnerPlayerCharacter)
+	{
+		FVector Velocity = OwnerPlayerCharacter->GetVelocity();
+		Speed = Velocity.Length();
+	}
+}
+
+void UCAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+}
---
+++ Source/Crunch/Public/Animations/CAnimInstance.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CAnimInstance.generated.h"
+
+class UCharacterMovementComponent;
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UCAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+	UFUNCTION(BlueprintPure, meta=(BlueprintThreadSafe))
+	FORCEINLINE float GetSpeed() const { return Speed; }
+
+	UFUNCTION(BlueprintPure, meta=(BlueprintThreadSafe))
+	FORCEINLINE bool IsMoving() const { return Speed != 0.f; }
+
+private:
+	UPROPERTY()
+	ACharacter* OwnerPlayerCharacter;
+
+	UPROPERTY()
+	UCharacterMovementComponent* OwnerMovementComp;
+
+	float Speed;
+};
