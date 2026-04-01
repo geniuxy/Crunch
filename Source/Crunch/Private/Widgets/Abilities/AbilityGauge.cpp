f59283953dbe9d71f2f8acdd88cff30d82ff0849
geniuxy <2238384910@qq.com>
2026-04-01 21:51:36 +0800

    93 Implement the Countdown Logic

--- Source/Crunch/Private/Widgets/Abilities/AbilityGauge.cpp
+++ Source/Crunch/Private/Widgets/Abilities/AbilityGauge.cpp
@@ -3,6 +3,7 @@
 
 #include "Widgets/Abilities/AbilityGauge.h"
 
+#include "AbilitySystemComponent.h"
 #include "Abilities/GameplayAbility.h"
 #include "Components/Image.h"
 #include "Components/TextBlock.h"
@@ -14,6 +15,16 @@ void UAbilityGauge::NativeConstruct()
 	Super::NativeConstruct();
 
 	CooldownCounterText->SetVisibility(ESlateVisibility::Hidden);
+
+	UAbilitySystemComponent* OwnerASC =
+		UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(GetOwningPlayerPawn());
+	if (OwnerASC)
+	{
+		OwnerASC->AbilityCommittedCallbacks.AddUObject(this, &ThisClass::AbilityCommit);
+	}
+
+	WholeNumberFormattingOptions.MaximumFractionalDigits = 0;
+	OneDigitNumberFormattingOptions.MaximumFractionalDigits = 1;
 }
 
 void UAbilityGauge::NativeOnListItemObjectSet(UObject* ListItemObject)
@@ -34,4 +45,54 @@ void UAbilityGauge::ConfigureWithWidgetData(const FAbilityWidgetData* WidgetData
 	{
 		Icon->GetDynamicMaterial()->SetTextureParameterValue(IconMaterialParamName, WidgetData->Icon.LoadSynchronous());
 	}
+}
+
+void UAbilityGauge::AbilityCommit(UGameplayAbility* Ability)
+{
+	if (Ability->GetClass()->GetDefaultObject() == AbilityCDO)
+	{
+		float CooldownTimeRemaining = 0.f;
+		float CooldownDuration = 0.f;
+
+		Ability->GetCooldownTimeRemainingAndDuration(
+			Ability->GetCurrentAbilitySpecHandle(),
+			Ability->GetCurrentActorInfo(),
+			CooldownTimeRemaining,
+			CooldownDuration
+		);
+
+		StartCooldown(CooldownTimeRemaining, CooldownDuration);
+	}
+}
+
+void UAbilityGauge::StartCooldown(float CooldownTimeRemaining, float CooldownDuration)
+{
+	CooldownDurationText->SetText(FText::AsNumber(CooldownDuration));
+	CachedCooldownDuration = CooldownDuration;
+	CachedCooldownTimeRemaining = CooldownTimeRemaining;
+
+	CooldownCounterText->SetVisibility(ESlateVisibility::Visible);
+
+	GetWorld()->GetTimerManager().SetTimer(
+		CooldownTimerHandle, this, &ThisClass::CooldownFinished, CooldownTimeRemaining
+	);
+	GetWorld()->GetTimerManager().SetTimer(
+		CooldownTimerUpdateHandle, this, &ThisClass::UpdateCooldown, CooldownUpdateInterval, true, 0.f
+	);
+}
+
+void UAbilityGauge::CooldownFinished()
+{
+	CachedCooldownDuration = 0.f;
+	CachedCooldownTimeRemaining = 0.f;
+	CooldownCounterText->SetVisibility(ESlateVisibility::Hidden);
+	GetWorld()->GetTimerManager().ClearTimer(CooldownTimerUpdateHandle);
+}
+
+void UAbilityGauge::UpdateCooldown()
+{
+	CachedCooldownTimeRemaining -= CooldownUpdateInterval;
+	FNumberFormattingOptions* FormattingOptions =
+		CachedCooldownTimeRemaining > 1.f ? &WholeNumberFormattingOptions : &OneDigitNumberFormattingOptions;
+	CooldownCounterText->SetText(FText::AsNumber(CachedCooldownTimeRemaining, FormattingOptions));
 }
--- Source/Crunch/Public/Widgets/Abilities/AbilityGauge.h
+++ Source/Crunch/Public/Widgets/Abilities/AbilityGauge.h
@@ -41,6 +41,25 @@ private:
 	UPROPERTY()
 	UGameplayAbility* AbilityCDO; // AbilityGauge所指向的Ability, CDO(Class Default Object)
 
+	void AbilityCommit(UGameplayAbility* Ability);
+
+	void StartCooldown(float CooldownTimeRemaining, float CooldownDuration);
+
+	void CooldownFinished();
+	void UpdateCooldown();
+
+	float CachedCooldownDuration;
+	float CachedCooldownTimeRemaining;
+
+	FTimerHandle CooldownTimerHandle;
+	FTimerHandle CooldownTimerUpdateHandle;
+
+	FNumberFormattingOptions WholeNumberFormattingOptions;
+	FNumberFormattingOptions OneDigitNumberFormattingOptions;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Cooldown")
+	float CooldownUpdateInterval = 0.1f;
+	
 	UPROPERTY(EditDefaultsOnly, Category="Visual")
 	FName IconMaterialParamName = "Icon";
 };
