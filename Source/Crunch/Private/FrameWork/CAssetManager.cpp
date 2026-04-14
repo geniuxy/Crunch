5c31190a38aa744285beb69983e6c939f2e91184
geniuxy <2238384910@qq.com>
2026-04-14 23:30:03 +0800

    134 Create the Asset Manager and ShopItem Loading Functions

---
+++ Source/Crunch/Private/FrameWork/CAssetManager.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FrameWork/CAssetManager.h"
+
+#include "CrunchDebugHelper.h"
+#include "CTypes/CStruct.h"
+
+UCAssetManager& UCAssetManager::Get()
+{
+	UCAssetManager* Singleton = Cast<UCAssetManager>(GEngine->AssetManager.Get());
+	if (Singleton)
+	{
+		return *Singleton;
+	}
+
+	Debug::Print(TEXT("AssetManager的类型需要是CAssetManager"));
+	return *NewObject<UCAssetManager>();
+}
+
+void UCAssetManager::LoadShopItems(const FStreamableDelegate& LoadFinishedCallback)
+{
+	LoadPrimaryAssetsWithType(
+		UPA_ShopItem::GetShopItemAssetType(),
+		TArray<FName>(),
+		FStreamableDelegate::CreateUObject(this, &ThisClass::ShopItemLoadFinished, LoadFinishedCallback)
+	);
+}
+
+bool UCAssetManager::GetLoadedShopItems(TArray<const UPA_ShopItem*>& OutItems) const
+{
+	TArray<UObject*> LoadedObjects;
+	bool bLoaded = GetPrimaryAssetObjectList(UPA_ShopItem::GetShopItemAssetType(), LoadedObjects);
+	if (bLoaded)
+	{
+		for (UObject* LoadedObject : LoadedObjects)
+		{
+			OutItems.Add(Cast<UPA_ShopItem>(LoadedObject));
+		}
+	}
+	return bLoaded;
+}
+
+void UCAssetManager::ShopItemLoadFinished(FStreamableDelegate Callback)
+{
+	Callback.ExecuteIfBound();
+	BuildItemMaps();
+}
+
+void UCAssetManager::BuildItemMaps()
+{
+	TArray<const UPA_ShopItem*> LoadedItems;
+	if (GetLoadedShopItems(LoadedItems))
+	{
+		for (const UPA_ShopItem* Item : LoadedItems)
+		{
+			if (Item->GetIngredients().Num() == 0) continue;
+
+			TArray<const UPA_ShopItem*> IngredientItems;
+			for (const TSoftObjectPtr<UPA_ShopItem>& Ingredient : Item->GetIngredients())
+			{
+				UPA_ShopItem* IngredientItem = Ingredient.LoadSynchronous();
+				IngredientItems.Add(IngredientItem);
+				AddToCombinationMap(IngredientItem, Item);
+			}
+
+			IngredientMap.Add(Item, FItemCollection(IngredientItems));
+		}
+	}
+}
+
+void UCAssetManager::AddToCombinationMap(const UPA_ShopItem* Ingredient, const UPA_ShopItem* CombinationItem)
+{
+	if (FItemCollection* Combinations = CombinationMap.Find(Ingredient))
+	{
+		if (!Combinations->Contains(CombinationItem))
+		{
+			Combinations->AddItem(CombinationItem);
+		}
+	}
+	else
+	{
+		CombinationMap.Add(Ingredient, FItemCollection(TArray{CombinationItem}));
+	}
+}
---
+++ Source/Crunch/Public/FrameWork/CAssetManager.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CTypes/CStruct.h"
+#include "Engine/AssetManager.h"
+#include "Inventory/Data/PA_ShopItem.h"
+#include "CAssetManager.generated.h"
+
+class UPA_ShopItem;
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UCAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+
+public:
+	static UCAssetManager& Get();
+
+	void LoadShopItems(const FStreamableDelegate& LoadFinishedCallback); // 异步加载ShopItems
+	bool GetLoadedShopItems(TArray<const UPA_ShopItem*>& OutItems) const; // 获取ShopItem的合集
+
+private:
+	void ShopItemLoadFinished(FStreamableDelegate Callback);
+	void BuildItemMaps();
+	void AddToCombinationMap(const UPA_ShopItem* Ingredient, const UPA_ShopItem* CombinationItem);
+	
+	UPROPERTY()
+	TMap<const UPA_ShopItem*, FItemCollection> CombinationMap; // Item的后续合成装备
+
+	UPROPERTY()
+	TMap<const UPA_ShopItem*, FItemCollection> IngredientMap; // Item的配件
+};
