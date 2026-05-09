8041a09326df89f6641b603767a7d3d4391f723b
geniuxy <2238384910@qq.com>
2026-05-09 20:40:06 +0800

    225 Make the CGameState class to Keep the Player Selections

---
+++ Source/Crunch/Private/FrameWork/CGameState.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FrameWork/CGameState.h"
+
+#include "CTypes/PlayerInfoTypes.h"
+#include "Net/UnrealNetwork.h"
+
+void ACGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(ACGameState, PlayerSelectionArray, COND_None, REPNOTIFY_Always);
+}
+
+void ACGameState::RequestPlayerSelectionChange(const APlayerState* RequestingPlayer, uint8 DesiredSlot)
+{
+	if (!HasAuthority() || IsSlotOccupied(DesiredSlot)) return;
+
+	FPlayerSelection* PlayerSelectionPtr = PlayerSelectionArray.FindByPredicate(
+		[&](const FPlayerSelection& PlayerSelection)
+		{
+			return PlayerSelection.IsForPlayer(RequestingPlayer);
+		}
+	);
+
+	if (PlayerSelectionPtr)
+	{
+		PlayerSelectionPtr->SetSlot(DesiredSlot);
+	}
+	else
+	{
+		PlayerSelectionArray.Add(FPlayerSelection(DesiredSlot, RequestingPlayer));
+	}
+
+	OnPlayerSelectionUpdated.Broadcast(PlayerSelectionArray);
+}
+
+bool ACGameState::IsSlotOccupied(uint8 SlotID) const
+{
+	for (const FPlayerSelection& PlayerSelection : PlayerSelectionArray)
+	{
+		if (PlayerSelection.GetPlayerSlot() == SlotID)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void ACGameState::OnRep_PlayerSelectionArray()
+{
+	OnPlayerSelectionUpdated.Broadcast(PlayerSelectionArray);
+}
---
+++ Source/Crunch/Public/FrameWork/CGameState.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "CGameState.generated.h"
+
+struct FPlayerSelection;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerSelectionUpdated, const TArray<FPlayerSelection>& /* NewPlayerSelection*/)
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API ACGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	void RequestPlayerSelectionChange(const APlayerState* RequestingPlayer, uint8 DesiredSlot);
+	bool IsSlotOccupied(uint8 SlotID) const;
+
+	FOnPlayerSelectionUpdated OnPlayerSelectionUpdated;
+
+private:
+	UPROPERTY(ReplicatedUsing=OnRep_PlayerSelectionArray)
+	TArray<FPlayerSelection> PlayerSelectionArray;
+
+	UFUNCTION()
+	void OnRep_PlayerSelectionArray();
+
+public:
+	FORCEINLINE const TArray<FPlayerSelection>& GetPlayerSelection() const { return PlayerSelectionArray; }
+};
